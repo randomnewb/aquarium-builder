c3a6c1dc684d1da0aee90a2fdad339ad6baa2096
randomnewb <nguy1581@gmail.com>
2023-07-09 19:50:37 -0500

    update pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,62 +1,71 @@
+// for use with neon serverless service
 const pg = require("pg");
-const url = require("url");
+require("dotenv").config();
 
-let config = {};
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `pg://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
-if (process.env.URL) {
-  const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-  const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+let config = {
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: true,
+  dialect: "pg",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+};
 
-  const sql = postgres(URL, { ssl: "require" });
+// if (process.env.DATABASE_URL) {
+//   // Heroku gives a url, not a connection object
+//   // https://github.com/brianc/node-pg-pool
+//   const params = url.parse(process.env.DATABASE_URL);
+//   const auth = params.auth.split(":");
 
-  async function getPgVersion() {
-    const result = await sql`select version()`;
-    console.log(result);
-  }
+//   config = {
+//     user: auth[0],
+//     password: auth[1],
+//     host: params.hostname,
+//     port: params.port,
+//     database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
+//     ssl: { rejectUnauthorized: false },
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+//   };
+// } else if (process.env.HEROKU_POSTGRESQL_GRAY_URL) {
+//   const params = url.parse(process.env.HEROKU_POSTGRESQL_GRAY_URL);
+//   const auth = params.auth.split(":");
+//   const host = params.host.split("@");
 
-  getPgVersion();
-}
-
-if (process.env.DATABASE_URL) {
-  // Heroku gives a url, not a connection object
-  // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(":");
-
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
-    ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
-} else if (process.env.HEROKU_POSTGRESQL_GRAY_URL) {
-  const params = url.parse(process.env.HEROKU_POSTGRESQL_GRAY_URL);
-  const auth = params.auth.split(":");
-  const host = params.host.split("@");
-
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
-    ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
-} else {
-  config = {
-    host: "localhost", // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: "prime_app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
-}
+//   config = {
+//     user: auth[0],
+//     password: auth[1],
+//     host: params.hostname,
+//     port: params.port,
+//     database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
+//     ssl: { rejectUnauthorized: false },
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+//   };
+// } else {
+//   config = {
+//     host: "localhost", // Server hosting the postgres database
+//     port: 5432, // env var: PGPORT
+//     database: "prime_app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+//   };
+// }
 
 // this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
